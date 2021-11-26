64aaec66b8af15cbe7661bcca87e582c95590ab4
spaseyhin <evgen.kalin99@gmail.com>
2021-11-26 17:46:19 +0500

    added tests on links

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,17 +5,17 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   @title = " Ruby on Rails Tutorial Sample App"
 end
   test "should get index" do
-    get home_index_url
+    get index_url
     assert_response :success #проверка сайта
     assert_select "title", "Index | Ruby on Rails Tutorial Sample App" #проверка тегов и их наличие
   end
   test "should get about" do
-    get home_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"#проверка тегов и их наличие
   end
   test "should get contact" do
-    get home_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
