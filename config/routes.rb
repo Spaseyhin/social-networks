074d0ba1ec9af7c1bd194bf20502a549865a6464
spaseyhin <evgen.kalin99@gmail.com>
2021-11-26 17:16:02 +0500

    changed link addresses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'article/new'
-  get 'home/index'
-  get 'home/about'
-  get 'home/contact'
+  get 'index' => 'home#index'
+  get 'about' => 'home#about'
+  get 'contact' => 'home#contact'
   root'ar#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
