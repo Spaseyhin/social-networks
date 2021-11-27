ec78b217357326c15b35df4a16a129b2d2579c39
spaseyhin <evgen.kalin99@gmail.com>
2021-11-27 23:00:11 +0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'index' => 'home#index'
   get 'about' => 'home#about'
   get 'contact' => 'home#contact'
+  get 'signup' => 'users#new'
   root'ar#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
