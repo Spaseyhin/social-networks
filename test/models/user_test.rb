81755ce237905d1716a1abd10b39dc31456dd4cf
spaseyhin <evgen.kalin99@gmail.com>
2021-12-07 10:56:12 +0500

    add test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,4 +7,31 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  test "name should be present" do 
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  test "email should be present" do 
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  test "email validation should accept valid addresses" do 
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_addresses|
+      @user.email = valid_addresses
+      assert @user.valid?, "#{valid_addresses.inspect} should be valid"
+    end
+  end
+    test "email validation should reject invalid addresses" do 
+    valid_addresses = %w[user@example,com USERfoo.com A_US-ER@foo. foo@bar_baz.com foo@bar+baz.com]
+    valid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be valid"
+    end
+  end
+  test "email addresses should be unique" do
+    buplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
