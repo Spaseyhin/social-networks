4ee2a76dfeed628eccb69c07e9fb4e89769df44d
spaseyhin <evgen.kalin99@gmail.com>
2021-12-11 13:07:47 +0500

    add registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'contact' => 'home#contact'
   get 'signup' => 'users#new'
   root'ar#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
