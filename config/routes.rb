26d16fa65712faea30cab06a945fa2491a12281e
spaseyhin <evgen.kalin99@gmail.com>
2022-01-03 14:02:08 +0500

    3,1 / 2

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get 'contact' => 'home#contact'
   get 'signup' => 'users#new'
   root'ar#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
