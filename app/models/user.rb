501878abd0a5e8fc7b166ff2ad0ee55ad225c924
spaseyhin <evgen.kalin99@gmail.com>
2022-01-19 19:40:10 +0500

    изменена название главной страницы в титуле

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+  # Возвращает дайджест для указанной строки
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
